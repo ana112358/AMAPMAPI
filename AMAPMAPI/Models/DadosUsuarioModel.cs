cea70b2f2c7d6d07fc5f18ad35630875e2e7d597
Ana Paula <ana.paula.ufrn.ti@gmail.com>
2025-09-27 10:46:35 -0300

    fix : cria model para os dados do usuário e para o usuário logado

---
+++ AMAPMAPI/Models/DadosUsuarioModel.cs
@@ -0,0 +1,28 @@
+﻿namespace AMAPMAPI.Models;
+public class DadosUsuarioModel
+{
+    public int Id { get; set; }
+    public int UsuarioId { get; set; } 
+    public string Nome { get; set; } = string.Empty;
+    public DateTime DataNascimento { get; set; }
+    public EnderecoModel Endereco { get; set; } = new EnderecoModel();
+    public List<AtividadeModel> Atividades { get; set; } = new List<AtividadeModel>();
+    public string TipoUsuario { get; set; } = string.Empty;
+}
+public class EnderecoModel
+{
+    public string Logradouro { get; set; } = string.Empty;
+    public string Numero { get; set; } = string.Empty;
+    public string Complemento { get; set; } = string.Empty;
+    public string Bairro { get; set; } = string.Empty;
+    public string Cidade { get; set; } = string.Empty;
+    public string Estado { get; set; } = string.Empty;
+    public string CEP { get; set; } = string.Empty;
+}
+public class AtividadeModel
+{
+    public int Id { get; set; }               
+    public string Descricao { get; set; } = string.Empty; 
+    public DateTime Data { get; set; } = DateTime.Now; 
+}
+
---
+++ AMAPMAPI/Models/UsuarioModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AMAPMAPI.Models;
+
+public class UsuarioModel
+{
+    public int ID { get; set; }
+    public string Email { get; set; }
+    public string Senha { get; set; }
+    public DateTime DataCriacao { get; set; } = DateTime.MinValue;
+}
+
